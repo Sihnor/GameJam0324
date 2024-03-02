9ba10a7508a741fad4d65f5b6398b56e893968fa
sihnor <m.rudolph.prgramming@outlook.com>
2024-03-02 13:19:52 +0100

    PLayer can move and look around

--- Assets/Prog/Marcel/Scripts/Heating.cs
+++ Assets/Prog/Marcel/Scripts/Heating.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using Prog.Marcel.Scripts;
@@ -5,8 +6,34 @@ using UnityEngine;
 
 public class Heating : MonoBehaviour, IHeatingUp
 {
+    [SerializeField] private float CurrentHeat = 0;
+    private const float MaxHeat = 100;
+
+    private bool IsHeatingUp = false;
+
+    private void Awake()
+    {
+        this.CurrentHeat = MaxHeat; 
+    }
+
     public void HeatingUp(float distance, float heatValue = 1)
     {
-        Debug.Log("Heating up: " + distance + " " + heatValue);
+        this.IsHeatingUp = true;
+        
+        if (this.CurrentHeat < MaxHeat)
+        {
+            this.CurrentHeat += heatValue;
+        }
+    }
+
+    private void FixedUpdate()
+    {
+        if (this.IsHeatingUp) return;
+        
+        if (this.CurrentHeat > 0)
+        {
+            this.CurrentHeat -= 0.1f;
+        }
+        
     }
 }
--- Assets/Prog/Marcel/Scripts/PlayerController.cs
+++ Assets/Prog/Marcel/Scripts/PlayerController.cs
@@ -1,38 +1,46 @@
 using System;
 using System.Collections;
 using System.Collections.Generic;
+using Unity.Mathematics;
 using UnityEngine;
 using UnityEngine.InputSystem;
 
 public class PlayerController : MonoBehaviour
 {
-     private InputAction MoveAction;
-     private InputAction LookMouseAction;
-     
-     private Vector2 MoveDirection;
+    [SerializeField] private float MouseXSensitivity = 1.0f;
+    [SerializeField] private float MouseYSensitivity = 1.0f;
+    [SerializeField] private float ControllerXSensitivity = 1.0f;
+    [SerializeField] private float ControllerYSensitivity = 1.0f;
+    [SerializeField] private float MovementSpeed = 1.0f;
 
-     private float KeyboardXSensitivity = 1.0f;
-     private float KeyboardYSensitivity = 1.0f;
-     private float ControllerXSensitivity = 1.0f;
-     private float ControllerYSensitivity = 1.0f;
-     [SerializeField] private float MovementSpeed = 1.0f;
-     
-     private Rigidbody PlayerRigidbody;
-    
+    private InputAction MoveAction;
+    private InputAction LookMouseAction;
+
+    private Vector2 MoveDirection;
+
+    [SerializeField] private Transform CameraTransform;
+
+    private Rigidbody PlayerRigidbody;
+
     private void Awake()
     {
-         this.MoveAction = GetComponent<PlayerInput>().currentActionMap.FindAction("MoveKeyboard");
-         this.LookMouseAction = GetComponent<PlayerInput>().currentActionMap.FindAction("LookMouse");
+        this.MoveAction = GetComponent<PlayerInput>().currentActionMap.FindAction("MoveKeyboard");
+        this.LookMouseAction = GetComponent<PlayerInput>().currentActionMap.FindAction("LookMouse");
+
+
+        Cursor.lockState = CursorLockMode.Locked;
+
     }
 
     // Start is called before the first frame update
     void Start()
     {
         this.MoveAction.performed += Move;
-        this.LookMouseAction.performed += ctx => Look(ctx, this.KeyboardXSensitivity, this.KeyboardYSensitivity);
-        
+        this.LookMouseAction.started += ctx => Look(ctx, this.MouseXSensitivity, this.MouseYSensitivity);
+        this.LookMouseAction.started += ctx => Look(ctx, this.ControllerXSensitivity, this.ControllerYSensitivity);
+
         this.MoveAction.canceled += ctx => this.MoveDirection = Vector2.zero;
-        
+
         this.PlayerRigidbody = GetComponentInChildren<Rigidbody>();
     }
 
@@ -42,20 +50,38 @@ public class PlayerController : MonoBehaviour
         this.MoveDirection.x *= this.MovementSpeed;
         this.MoveDirection.y *= this.MovementSpeed;
     }
-    
+
     private void Look(InputAction.CallbackContext ctx, float xSensitivity, float ySensitivity)
     {
         Vector2 look = ctx.ReadValue<Vector2>();
+
         look.x *= xSensitivity;
-        look.y *= ySensitivity;
+        look.y *= ySensitivity * -1;
+        transform.Rotate(Vector3.up, look.x);
+
+        float eulerX = this.CameraTransform.eulerAngles.x;
         
-        Debug.Log("Look: " + look);
+        // Clamp the camera rotation
+        if (this.CameraTransform.eulerAngles.x <= 70.1 && this.CameraTransform.eulerAngles.x + look.y > 70 )
+        {
+            eulerX = 70;
+        }
+        else if (this.CameraTransform.eulerAngles.x >= 289.9 && this.CameraTransform.eulerAngles.x + look.y < 360 - 70)
+        {
+            eulerX = 360 - 70;
+        }else
+        {
+            eulerX += look.y;
+        }
+        
+        this.CameraTransform.rotation = Quaternion.Euler(eulerX, this.CameraTransform.eulerAngles.y, 0);
     }
 
     private void FixedUpdate()
     {
-        Vector3 velocity = this.MoveDirection.x * transform.forward + this.MoveDirection.y * transform.right;
-        
+        Vector3 velocity = this.MoveDirection.x * transform.right + this.MoveDirection.y * transform.forward;
+
         this.PlayerRigidbody.AddForce(velocity);
+        Debug.Log(velocity);
     }
 }
